1ac525e7b26cd3880a5c2bb01cc9af5ed96ff013
Serhii Starinskyi <starinskylife@gmail.com>
2021-08-15 12:29:41 +0300

    new commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static(__dirname + "/dist/epam-ang"));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/epam-ang/index.html"));
+});
+
+app.listen(process.env.PORT || 8000);
